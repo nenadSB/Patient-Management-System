45588f88edce9c0a6347adc219b68875b4904e56
nenadSB <nenads85@.com>
2024-11-01 17:40:40 +0100

    Sixth  commit

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,22 +1,36 @@
+import { AuthService } from './auth/auth.service';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RouterLink, RouterLinkActive , provideRouter} from '@angular/router';
+import { RouterLink, RouterLinkActive, provideRouter } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
+import { Router } from '@angular/router'; // Import Router for navigation
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports:
-  [
+  imports: [
     RouterOutlet,
     CommonModule,
     RouterLink,
-    RouterLinkActive],
-
+    RouterLinkActive
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'] // Corrected to styleUrls
 })
 export class AppComponent {
   title = 'angular-frontend';
+
+  // Inject AuthService and Router
+  constructor(private authService: AuthService, private router: Router) {}
+
+  // Method to log out
+  logout(): void {
+    this.authService.logout(); // Call logout method from AuthService
+    this.router.navigate(['/login']); // Redirect to login page
+  }
+
+  // Method to check if the user is authenticated
+  isAuthenticated(): boolean {
+    return this.authService.getAuthStatus();
+  }
 }
